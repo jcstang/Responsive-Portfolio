6b5dbc93ae11d8fc8b4631eccf48d69e7f5ffeeb
Jacob Stanger <jacobcstanger@gmail.com>
2020-02-20 10:45:03 -0700

    good stuff now with jquery hover shadow and link to recipe app.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,17 @@
 // Jacob Stangers Portfolio
 
 $(document).ready(function () {
+
   $('.work').on('click', function() {
-    console.log(this);
-    console.log('was pressed');
-    
+    // console.log(this);
+    console.log(this + ' was pressed');
+
     
   });
+
+//   $('button selector').click(function(){
+//     window.location.href='the_link_to_go_to.html';
+//  })
 
   // $('.project-box').on('click', 'div', function() {});
 
@@ -17,12 +22,25 @@ $(document).ready(function () {
   //     $( this ).css('box-shadow', ' ');
   //   }
   // );
-  $( ".work" ).hover(
-    function() {
-      $( this ).toggle('shadow');
-    }, function() {
-      $( this ).toggleClass('shadow');
-    }
-  );
+
+  // cool disappering trick!!!
+  // $( ".work" ).hover(
+  //   function() {
+  //     $( this ).toggle('shadow');
+  //   }, function() {
+  //     $( this ).toggleClass('shadow');
+  //   }
+  // );
+
+
+  $(".work").hover(function() {
+    $(this).css(
+        "box-shadow", "7px 7px 15px #888"
+    );
+}, function() {
+    $(this).css(
+        "box-shadow", "0px 0px 0px #888"
+    );
+});
   
 });
